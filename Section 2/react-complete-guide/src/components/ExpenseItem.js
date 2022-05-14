91785517aec8f029ebd4e10975e47769d9e4f9d2
Baumel Márton <marcibaumel@gmail.com>
2022-05-14 15:11:54 +0200

    35

--- Section 2/react-complete-guide/src/components/ExpenseItem.js
+++ Section 2/react-complete-guide/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './ExpenseItem.css'
 
 function ExpenseItem() {
     return (
-        <>
+        <div className='expense-item'>
             <div>March 28</div>
-            <div>
-                <h2>Car</h2>
-                <div>294.3</div>
+            <div className='expense-item__description'>
+                <h2>Car Price</h2>
+                <div className='expense-item__price'>294.3</div>
             </div>
-        </>
+        </div>
     )
 }
 
