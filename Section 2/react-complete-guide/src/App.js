a54c9d000fa5eb263a9578e25125f9467488bc12
Baumel Márton <marcibaumel@gmail.com>
2022-05-14 14:57:20 +0200

    33

--- Section 2/react-complete-guide/src/App.js
+++ Section 2/react-complete-guide/src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <h2>Let's get started </h2>
       <p>This is visibklie too </p>
+      <ExpenseItem/>
     </div>
   );
 }
