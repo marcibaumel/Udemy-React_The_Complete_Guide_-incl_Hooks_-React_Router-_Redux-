6d4f48307441a5d366631afb3de868ad3e03dfc4
Baumel Márton <marcibaumel@gmail.com>
2022-05-15 10:49:56 +0200

    36

--- Section 2/react-complete-guide/src/components/ExpenseItem.js
+++ Section 2/react-complete-guide/src/components/ExpenseItem.js
@@ -2,12 +2,17 @@ import React from 'react'
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+    const expenseDate= new Date(2021, 2, 28);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 290.3;
+
     return (
         <div className='expense-item'>
-            <div>March 28</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Price</h2>
-                <div className='expense-item__price'>294.3</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}$</div>
             </div>
         </div>
     )
