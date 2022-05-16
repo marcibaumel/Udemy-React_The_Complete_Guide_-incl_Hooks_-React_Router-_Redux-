1cc05b5d91da23c269f6bc5dfebd020434f8a2f8
Baumel Márton <marcibaumel@gmail.com>
2022-05-16 16:40:23 +0200

    47

--- Section 2/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Section 2/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,11 @@ import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+
+    const clickHandler = () => {
+        console.log('Clicked')
+    }
+
     return (
         <Card className='expense-item'>
            <ExpenseDate date={props.date}/>
@@ -11,6 +16,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}$</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
