4efb4f62c7a7620cc7516483e0bd764170b9a6a8
Baumel Márton <marcibaumel@gmail.com>
2022-05-17 19:38:27 +0200

    Section 4

--- Section 2/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Section 2/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
+
 
 const ExpenseItem = (props) => {
 
@@ -13,14 +14,18 @@ const ExpenseItem = (props) => {
     }
 
     return (
-        <Card className='expense-item'>
-           <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>{props.amount}$</div>
-            </div>
-            <button onClick={clickHandler}>Change title</button>
-        </Card>
+        <>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>{props.amount}$</div>
+                </div>
+                <button onClick={clickHandler}>Change title</button>
+            </Card>
+
+        </>
+
     )
 }
 
