35ca437d522a04a9c52d895d1da2a0661de2cab2
Baumel Márton <marcibaumel@gmail.com>
2022-05-23 10:02:08 +0200

    93

--- user_age/src/components/Users/AddUser.js
+++ user_age/src/components/Users/AddUser.js
@@ -25,9 +25,9 @@ export const AddUser = (props) => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" onChange={usernameChangeHandler} />
                 <label htmlFor='age'>Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
