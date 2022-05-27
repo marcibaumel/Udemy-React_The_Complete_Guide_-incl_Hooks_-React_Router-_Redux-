8addf8814b588cfdd0ef70f4b526ef9377d58020
Baumel Márton <marcibaumel@gmail.com>
2022-05-27 09:18:57 +0200

    104

--- user_age/src/App.js
+++ user_age/src/App.js
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AddUser } from './components/Users/AddUser';
 import { UserList } from './components/Users/UserList';
 function App() {
 
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge)=> {
+  const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
     });
-  } 
+  }
 
   return (
     <>
-        <AddUser onAddUser={addUserHandler}/>
-        <UserList users={usersList}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </>
   );
 }
