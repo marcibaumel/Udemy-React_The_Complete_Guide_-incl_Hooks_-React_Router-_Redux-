b513ed0465b73121f50064d697d1a87436149a1f
Baumel Márton <marcibaumel@gmail.com>
2022-05-29 21:48:06 +0200

    114

--- side_effects/src/components/Login/Login.js
+++ side_effects/src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    
+    //THE CLEANUP FUNCTION
+    return () => {
+      clearTimeout(identifier);
+    };
+
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
