1c2a14c5f776513d7a5f055cd82822e0418089cf
Baumel Márton <marcibaumel@gmail.com>
2022-06-07 16:58:18 +0200

    134

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import { HeaderCartButton } from './HeaderCartButton'
 
 export const Header = () => {
     return (
         <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton></HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='MealsImg' />
