8591b94c211d6d8c70e2b968e6524bf2c07cf8eb
Baumel Márton <marcibaumel@gmail.com>
2022-06-10 14:25:04 +0200

    140

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,14 +1,16 @@
 import { Header } from "./components/Layout/Header";
 import { Meals } from "./components/Meals/Meals";
-
+import { Cart } from "./components/Cart/Cart";
+import {Fragment} from 'react'
 function App() {
   return (
-    <>
+    <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
       </main>
-    </>
+    </Fragment>
   );
 }
 
--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import classes from './Cart.module.css'
+import { Modal } from '../UI/Modal';
 
 export const Cart = (props) => {
     const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'su', amount: 2, price: 12.88 }].map(item =>
         <li>{item.name}</li>)}</ul>;
 
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total AMount</span>
                 <span>35.6</span>
             </div>
-            <div className={classes.action}>
+            <div className={classes.actions}>
                 <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 }
