56eea5163ad7cd389614c3f464945272268e8234
Baumel Márton <marcibaumel@gmail.com>
2022-06-10 12:54:36 +0200

    136

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
+import { Card } from '../UI/Card';
+import { MealItem } from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -30,15 +32,17 @@ const DUMMY_MEALS = [
 
 export const AvailableMeals = () => {
 
-    const mealList = DUMMY_MEALS.map((meal) => 
-        <li>{meal.name}</li>
+    const mealList = DUMMY_MEALS.map((meal) =>
+        <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
     );
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealList}
+                </ul>
+            </Card>
         </section>
     )
 }
