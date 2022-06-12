02a053f5baa172f523d4890f7054b4f0d0821fda
Baumel Márton <marcibaumel@gmail.com>
2022-06-12 17:53:31 +0200

    144

--- food-order-app/src/components/Meals/MealItem/MeaItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MeaItemForm.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import classes from './MealItemForm.module.css'
 import { Input } from '../../UI/Input'
 
 export const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const submitHandler = event => {
+    event.preventDefault();
+
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input label="Amount" input={{
-       id: 'amount_' + props.id, 
-       type: 'number',
-       min: '1',
-       max: '5',
-       step: '1',
-       defaultValue: '1',
-      }}/>
+        ref: { amountInputRef },
+        id: 'amount_' + props.id,
+        type: 'number',
+        min: '1',
+        max: '5',
+        step: '1',
+        defaultValue: '1',
+      }} />
       <button>+ Add</button>
     </form>
   )
--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cart-context'
+
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD_ITEM'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount.action.item.price * action.item.amount;
+        return{
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    if(action.type === 'REMOVE_ITEM'){
+
+    }
+    return defaultCartState;
+}
 
 export const CartProvider = (props) => {
 
-    const addItemCartHandler = (item) => {
+    const [cartState, dispatchCartAction]=useReducer(cartReducer, defaultCartState);
 
+    const addItemCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD_ITEM', item: item});
     }
 
     const removeItemFromCartHandler = (id) => {
-
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id});
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemCartHandler,
         removeItem: removeItemFromCartHandler 
     };
