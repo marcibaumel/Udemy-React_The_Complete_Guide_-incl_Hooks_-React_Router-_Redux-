141af9fba200bc85bc3d4beb104be466a5f8a299
Baumel Márton <marcibaumel@gmail.com>
2022-06-13 17:44:15 +0200

    152

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -9,7 +9,7 @@ export const Header = (props) => {
         <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton onClick={props.onShowCart}/>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='MealsImg' />
