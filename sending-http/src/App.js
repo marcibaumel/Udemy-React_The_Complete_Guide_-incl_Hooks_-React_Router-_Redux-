c297dfeed2495c27908c6b15d8464961b877ec20
Baumel Márton <marcibaumel@gmail.com>
2022-08-19 10:53:39 +0200

    181

--- sending-http/src/App.js
+++ sending-http/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -35,15 +35,19 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>
 
-  if(movies.length>0){
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />
   }
 
-  if (error){
+  if (error) {
     content = <p>{error}</p>
   }
 
