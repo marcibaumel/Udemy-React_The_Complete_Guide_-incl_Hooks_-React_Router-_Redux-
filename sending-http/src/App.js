a2e32c6cb34d22664b44167de4219ef6a643deb2
Baumel Márton <marcibaumel@gmail.com>
2022-08-19 10:10:49 +0200

    179

--- sending-http/src/App.js
+++ sending-http/src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json();
 
@@ -19,8 +21,8 @@ function App() {
         releaseDate: movieData.release_date
       }
     });
-
     setMovies(transformedMovies)
+    setIsLoading(false);
   }
 
   return (
@@ -29,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading ? <MoviesList movies={movies} /> : <p>Loading</p>}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
       </section>
     </React.Fragment>
   );
