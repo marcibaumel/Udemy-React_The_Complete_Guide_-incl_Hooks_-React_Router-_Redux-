d27e07210f61e685222e827a6066ed9f883cd359
Baumel Márton <marcibaumel@gmail.com>
2022-08-22 16:56:23 +0200

    191

--- onwards-to-a-more-realistic-example-custom-hook/src/components/NewTask/NewTask.js
+++ onwards-to-a-more-realistic-example-custom-hook/src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        process.env.REACT_APP_FIREBASE_URL,
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
