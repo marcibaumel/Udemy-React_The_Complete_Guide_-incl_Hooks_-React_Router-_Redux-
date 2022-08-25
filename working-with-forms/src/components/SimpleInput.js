b50edcbfbc848bd500cd56b933916f175ea85a6c
Baumel Márton <marcibaumel@gmail.com>
2022-08-25 13:44:22 +0200

    26

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -1,13 +1,21 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const SimpleInput = (props) => {
+
     const [enteredName, setEnteredName] = useState('');
     const [enteredNameTouch, setEnteredNameTouch] = useState(false);
+
 
     const enteredNameIsValid = enteredName.trim() !== '';
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouch;
     const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
-    
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid) {
+        formIsValid = true;
+    }
+
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
     }
@@ -35,16 +43,16 @@ const SimpleInput = (props) => {
             <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input
-                       type='text'
-                       id='name'
-                       onChange={nameInputChangeHandler}
-                       onBlur={nameInputBluerHandler}
-                       value={enteredName}
+                    type='text'
+                    id='name'
+                    onChange={nameInputChangeHandler}
+                    onBlur={nameInputBluerHandler}
+                    value={enteredName}
                 />
                 {nameInputIsInvalid && <p className="error-text">Not must not be empty</p>}
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );
