3b96199cddabeb19ba5c71ddd2c3df6a6c6e5993
Baumel Márton <marcibaumel@gmail.com>
2022-08-26 10:40:22 +0200

    214

---
+++ http-form-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from 'react'
+import CartIcon from '../Cart/CartIcon'
+import CartContext from '../../store/cart-context'
+import classes from './HeaderCartButton.module.css'
+
+export const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>
+        {numberOfCartItems}
+      </span>
+    </button>
+  )
+}
