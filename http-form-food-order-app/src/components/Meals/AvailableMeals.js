d693349184701bf206e3601e14e5816460321383
Baumel Márton <marcibaumel@gmail.com>
2022-08-28 19:42:58 +0200

    Update AvailableMeals.js

--- http-form-food-order-app/src/components/Meals/AvailableMeals.js
+++ http-form-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
-import { Card } from '../UI/Card';
-import { MealItem } from './MealItem/MealItem';
+import {Card} from '../UI/Card';
+import {MealItem} from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
