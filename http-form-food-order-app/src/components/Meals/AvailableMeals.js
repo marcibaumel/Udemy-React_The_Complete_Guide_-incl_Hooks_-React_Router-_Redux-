d8eb0c5396521861c49e8a6dc4269be00845a7f7
Baumel Márton <marcibaumel@gmail.com>
2022-09-05 20:06:09 +0200

    216

--- http-form-food-order-app/src/components/Meals/AvailableMeals.js
+++ http-form-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import React from 'react'
 import classes from './AvailableMeals.module.css'
 import {Card} from '../UI/Card';
@@ -5,36 +6,34 @@ import {MealItem} from './MealItem/MealItem';
 
 //process.env.REACT_APP_FIREBASE_URL
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 
 export const AvailableMeals = () => {
 
-    const mealList = DUMMY_MEALS.map((meal) =>
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(process.env.REACT_APP_FIREBASE_URL);
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+
+            setMeals(loadedMeals);
+        };
+
+        fetchMeals();
+    }, []);
+
+    const mealList = meals.map((meal) =>
         <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
     );
 
