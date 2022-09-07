2d4c5fe2dbff85042dd243a4b297ca3d869246a6
Baumel Márton <marcibaumel@gmail.com>
2022-09-07 18:41:03 +0200

    224

--- http-form-food-order-app/src/store/cart-context.js
+++ http-form-food-order-app/src/store/cart-context.js
@@ -3,9 +3,12 @@ import React from 'react'
 const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
-    addItem: (item) => {},
-    removeItem: (id) => {},
-    clearCart: () => {}
+    addItem: (item) => {
+    },
+    removeItem: (id) => {
+    },
+    clearCart: () => {
+    }
 })
 
 export default CartContext;
