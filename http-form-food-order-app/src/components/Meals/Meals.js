a3cbcce13a1892803b63b10fefb0480b77d27f49
Baumel Márton <marcibaumel@gmail.com>
2022-09-07 18:41:15 +0200

    225

--- http-form-food-order-app/src/components/Meals/Meals.js
+++ http-form-food-order-app/src/components/Meals/Meals.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import MealsSummary from './MealsSummary'
-import { AvailableMeals } from './AvailableMeals'
+import {AvailableMeals} from './AvailableMeals'
 
 export const Meals = () => {
-  return (
-    <>
-        <MealsSummary/>
-        <AvailableMeals/>
-    </>
-  )
+    return (
+        <>
+            <MealsSummary/>
+            <AvailableMeals/>
+        </>
+    )
 }
