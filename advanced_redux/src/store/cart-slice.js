69de6143e937aa1912cd5c5a0d4a939a96ab177c
Baumel Márton <marcibaumel@gmail.com>
2022-09-24 12:12:52 +0200

    261

--- advanced_redux/src/store/cart-slice.js
+++ advanced_redux/src/store/cart-slice.js
@@ -8,6 +8,10 @@ const cartSlice = createSlice({
         totalQuantity: 0
     },
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
@@ -39,47 +43,6 @@ const cartSlice = createSlice({
     }
 });
 
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(
-            uiActions.showNotification({
-                status: 'pending',
-                title: 'Sending',
-                message: 'Sending cart data!'
-            })
-        );
-
-        const sendRequest = async () => {
-            const response = await fetch('https://react-http-1bf8b-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-                {
-                    method: 'PUT',
-                    body: JSON.stringify(cart)
-                });
-            if (!response.ok) {
-                throw new Error('Sending cart data error');
-                //return
-            }
-        }
-
-        try {
-            await sendRequest();
-
-            dispatch(uiActions.showNotification({
-                status: 'success',
-                title: 'Success!',
-                message: 'Sending cart data successfully!'
-            }))
-        } catch (error) {
-            dispatch(uiActions.showNotification({
-                status: 'error',
-                title: 'Error!',
-                message: 'Sending cart data failed!'
-            }))
-        }
-
-
-    }
-}
 
 export const cartAction = cartSlice.actions;
 export default cartSlice;
