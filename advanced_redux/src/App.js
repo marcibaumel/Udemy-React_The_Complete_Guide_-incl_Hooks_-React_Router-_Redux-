1a7628e99a7b374ce2d0812030811b9fd20d518d
Baumel Márton <marcibaumel@gmail.com>
2022-09-24 12:30:30 +0200

    265

--- advanced_redux/src/App.js
+++ advanced_redux/src/App.js
@@ -15,17 +15,17 @@ function App() {
     const cart = useSelector(state => state.cart);
     const notification = useSelector(state => state.ui.notification);
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(fetchCartData())
     }, [dispatch])
 
     useEffect(() => {
-        if(isInitial){
-            isInitial=false;
+        if (isInitial) {
+            isInitial = false;
             return
         }
 
-        if(cart.changed){
+        if (cart.changed) {
             dispatch(sendCartData(cart));
         }
     }, [cart, dispatch])
