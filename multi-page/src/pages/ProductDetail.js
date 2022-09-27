e182f67fdc9320b55f702b6da40401d3ccd4908b
Baumel Márton <marcibaumel@gmail.com>
2022-09-27 21:59:02 +0200

    273

--- multi-page/src/pages/ProductDetail.js
+++ multi-page/src/pages/ProductDetail.js
@@ -1,7 +1,15 @@
+import {useParams} from 'react-router-dom'
+
 export const ProductDetail = () => {
+
+    const params = useParams();
+
+    console.log(params.productId)
+
     return (
         <>
             <h1>Product detail</h1>
+            <p>{params.productId}</p>
         </>
     )
 }
