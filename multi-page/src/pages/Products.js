2fe58eaebc118f82da9f8b871f537972aaebdd8b
Baumel Márton <marcibaumel@gmail.com>
2022-09-28 15:40:03 +0200

    274

--- multi-page/src/pages/Products.js
+++ multi-page/src/pages/Products.js
@@ -1,11 +1,13 @@
+import {Link} from "react-router-dom";
+
 export const Products = () => {
     return(
     <section>
         <h1>The products Page</h1>
         <ul>
-            <li>Book</li>
-            <li>Carpet</li>
-            <li>Film</li>
+            <li><Link to='products/1'>Book</Link></li>
+            <li><Link to='products/2'>Carpet</Link></li>
+            <li><Link to='products/3'>Film</Link></li>
         </ul>
     </section>
     )
