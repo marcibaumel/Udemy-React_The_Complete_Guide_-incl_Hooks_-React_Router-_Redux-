736ab7ba37be06959a4a06e643a528a6942bd78d
Baumel Márton <marcibaumel@gmail.com>
2022-09-28 16:14:04 +0200

    275

--- multi-page/src/pages/Welcome.js
+++ multi-page/src/pages/Welcome.js
@@ -1,3 +1,12 @@
+import {Route} from "react-router-dom";
+
 export const Welcome = () => {
-    return <h1>The welcome page</h1>
+    return (
+        <>
+            <h1>The welcome page</h1>
+            <Route path='/welcome/new-user'>
+                <p>Welcome new user</p>
+            </Route>
+        </>
+    )
 }
