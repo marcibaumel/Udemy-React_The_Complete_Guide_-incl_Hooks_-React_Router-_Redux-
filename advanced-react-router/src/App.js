9c9c6a3963c0e45669048728604fbac45dc3e810
Baumel Márton <marcibaumel@gmail.com>
2022-09-30 09:52:28 +0200

    280

--- advanced-react-router/src/App.js
+++ advanced-react-router/src/App.js
@@ -2,24 +2,27 @@ import {Route, Switch} from "react-router-dom";
 import {AllQuotes} from "./components/pages/AllQuotes";
 import {QuoteDetail} from "./components/pages/QuoteDetail";
 import {NewQuote} from "./components/pages/NewQuote";
+import {Layout} from "./components/layout/Layout";
 import Redirect from "react-router-dom/es/Redirect";
 
 function App() {
     return (
-        <Switch>
-            <Route path='/' exact>
-                <Redirect to='/quotes'/>
-            </Route>
-            <Route path='/quotes' exact>
-                <AllQuotes/>
-            </Route>
-            <Route path='/quotes/:quoteId'>
-                <QuoteDetail/>
-            </Route>
-            <Route path='/new-quote'>
-                <NewQuote/>
-            </Route>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <Redirect to='/quotes'/>
+                </Route>
+                <Route path='/quotes' exact>
+                    <AllQuotes/>
+                </Route>
+                <Route path='/quotes/:quoteId'>
+                    <QuoteDetail/>
+                </Route>
+                <Route path='/new-quote'>
+                    <NewQuote/>
+                </Route>
+            </Switch>
+        </Layout>
 
     );
 }
--- advanced-react-router/src/components/pages/AllQuotes.js
+++ advanced-react-router/src/components/pages/AllQuotes.js
@@ -1,3 +1,15 @@
+import QuoteList from "../quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'me', text: 'React is fun'},
+    {id: 'q2', author: 'me again', text: 'React is fun2'},
+    {id: 'q3', author: 'me', text: 'React is fun3'},
+    {id: 'q4', author: 'me again', text: 'React is fun4'},
+]
+
+
 export const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return (
+    <QuoteList quotes={DUMMY_QUOTES}></QuoteList>
+    )
 }
