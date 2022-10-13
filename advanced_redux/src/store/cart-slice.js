d8cc9257f49e85de9b1d7573fdabda25e5789a79
Baumel Márton <marcibaumel@gmail.com>
2022-10-13 10:02:47 +0200

    290

--- advanced_redux/src/store/cart-slice.js
+++ advanced_redux/src/store/cart-slice.js
@@ -2,7 +2,7 @@ import {createSlice} from "@reduxjs/toolkit";
 import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
-    name: 'cart',
+name: 'cart',
     initialState: {
         items: [],
         totalQuantity: 0,
